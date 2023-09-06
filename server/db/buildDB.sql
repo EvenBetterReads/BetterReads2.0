26e37763483c2f46fa71f56b918d080239aee262
CD Robinson <cd@christian-daniel.com>
2023-09-06 13:56:44 -0700

    new server file and updated routes

--- server/db/buildDB.sql
+++ server/db/buildDB.sql
@@ -18,6 +18,7 @@ CREATE TABLE IF NOT EXISTS users(
 --Book Review table
 CREATE TABLE IF NOT EXISTS book_review(
     _id serial NOT NULL,
+    user_id bigint NOT NULL,
     title varchar NOT NULL,
     author varchar NOT NULL,
     genre varchar NOT NULL,
@@ -42,15 +43,4 @@ ALTER TABLE users ADD CONSTRAINT "unique_username" UNIQUE ("username");
 Book Review:
     - group_id
 */
-ALTER TABLE book_review ADD CONSTRAINT "book_review_fk0" FOREIGN KEY ("user_id") REFERENCES user("_id");
-
-/*
-Travel Group Members:
-    - user_id: users
-    - group_id: travel_group
-*/
-
-ALTER TABLE group_members ADD CONSTRAINT "group_members_fk0" FOREIGN KEY ("user_id") REFERENCES users("_id");
-ALTER TABLE group_members ADD CONSTRAINT "group_members_fk1" FOREIGN KEY ("group_id") REFERENCES travel_group("_id");
--- Add UNIQUE constraint of user_id + group_id
-ALTER TABLE group_members ADD CONSTRAINT "unique_user_group" UNIQUE ("user_id", "group_id");
+ALTER TABLE book_review ADD CONSTRAINT "book_review_fk0" FOREIGN KEY ("user_id") REFERENCES users("_id");
